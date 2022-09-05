f09223753a726c444f8e8fb5bb3c16037802d052
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-05 20:02:14 +1000

    db schema update

--- database/schema.sql
+++ database/schema.sql
@@ -1,6 +1,36 @@
-
 CREATE TABLE users (
   id SERIAL PRIMARY KEY,
   email TEXT UNIQUE,
   password TEXT
 );
+
+CREATE TABLE holidays (
+  id SERIAL PRIMARY KEY,
+  user_id INT,
+  holiday_name TEXT,
+  date_start DATE,
+  date_end DATE,
+  location_name TEXT,
+  img_link TEXT
+)
+
+CREATE TABLE holiday_parts (
+  id SERIAL PRIMARY KEY,
+  holiday_id INT, 
+  part_name TEXT,
+  date_start DATE,
+  date_end DATE,
+  location_name TEXT,
+  img_link TEXT,
+)
+
+CREATE TABLE events (
+  id SERIAL PRIMARY KEY,
+  part_id INT, 
+  event_name TEXT,
+  location_name TEXT,
+  img_link TEXT,
+  event_description TEXT, 
+)
+
+
