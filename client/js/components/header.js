885945014f69ff0287f334368ae8eb38b6cead5a
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-07 19:12:39 +1000

    quickly update header html :) approval has been given too lol

--- client/js/components/header.js
+++ client/js/components/header.js
@@ -17,6 +17,7 @@ const renderHeader = (isLoggedIn, user) => {
 		header.innerHTML = `
      <h1><a href="/">Travel Diary</a></h1>
      <ul id="navlist">
+	 
         <button onClick="renderNewTrip()">New Trip</button>
         <li><a onClick="renderProfile()">Profile</a></li>
         <li><a onClick="logOut()">Logout</a></li>
