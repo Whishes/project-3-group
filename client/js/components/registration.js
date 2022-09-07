04ba668fec59f956f8069a941ffad028be00e4cf
Elise James <elisabethmariajames@gmail.com>
2022-09-07 21:57:50 +1000

    Axios post request added to registration form

--- client/js/components/registration.js
+++ client/js/components/registration.js
@@ -8,7 +8,7 @@ const renderRegistrationForm = () => {
                 <p>Register to continue</p>
                 <section id="error-msgs"></section>
                 <fieldset>
-                    <label for="first-name">First Name</label>
+                    <label for="firstname">First Name</label>
                     <input type="text" name="first-name">
                 </fieldset>
                 <fieldset>
@@ -27,5 +27,23 @@ const renderRegistrationForm = () => {
             </form>
             <p>Already have an account?</p> <button>Login</button>
         </section>`};
+
+        const form = document.getElementById("registration-form");
+        form.addEventListener("submit", (event) => {
+            event.preventDefault();
+            const formData = new FormData(form);
+            const data = {
+                // firstName: formData.get("firstname"),
+                // surname: formData.get("surname"),
+                email: formData.get("email"),
+                password: formData.get("password")
+            };
+            axios.post("api/signup", data).then(() => {
+                location = "/";
+            }).catch((err) => {
+            alert(err.response.data.message);
+            });
+        });
+       
 
 module.exports = renderRegistrationForm;
