681c633bf4d47ee620f2ed81515b91d46aa07188
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-08 16:50:02 +1000

    fixed a weird session.id bug that caused the userID not going into the req.session object correctly

--- client/js/components/homePage.js
+++ client/js/components/homePage.js
@@ -2,12 +2,19 @@ const renderHomePage = (isLoggedIn) => {
 	const sectionPage = document.getElementById("page");
 	const container = document.createElement("div");
 	container.id = "content-container";
+
+	//
 	if (isLoggedIn) {
 		// user is logged in
 		container.innerHTML = `
 		<h1 id="oops">Oops! Looks like there's no content...</h1>
-		<div id="subheading-container"><h2 id="subheading">Add new holiday?</h2><button onClick="renderNewTrip()">New Trip</button></div>
+		<div id="subheading-container">
+			<h2 id="subheading">Add new holiday?</h2>
+			<button onClick="renderNewTrip()">New Trip</button>
+		</div>
 		`;
+
+		
 	} else {
 		// user isn't logged in
 		container.innerHTML = `<h2>User isn't logged in</h2>`;
