7c67e51f555fe6a75890c10e44f2619073db08e8
Elise James <elisabethmariajames@gmail.com>
2022-09-09 20:18:45 +1000

    Updated back-end controller/model & SQL query to post & insert all user database fields

--- controllers/signup.js
+++ controllers/signup.js
@@ -5,13 +5,12 @@ const Signup = require("../models/signup");
 
 // signup stuff
 router.post("/", (req, res) => {
-	//console.log("here");
-	console.log(req.body);
-	const { email, password } = req.body;
+	// console.log(req.body)
+	const { firstname, surname, username, email, password } = req.body;
 
 	const hashedPassword = generateHash(password);
 
-	Signup.newAcc(email, hashedPassword)
+	Signup.newAcc(firstname, surname, username, email, hashedPassword)
 		.then(() => res.status(200).json({"success": true}))
 		.catch((err) => res.sendStatus(500));
 });
--- models/signup.js
+++ models/signup.js
@@ -1,9 +1,9 @@
 const db = require("../database/db.js");
 
 const Signup = {
-	newAcc: (email, hashedPassword) => {
-		const sql = `INSERT INTO users(email, password) VALUES($1, $2)`;
-		return db.query(sql, [email, hashedPassword]).then((dbRes) => dbRes);
+	newAcc: (firstname, surname, username, email, hashedPassword) => {
+		const sql = `INSERT INTO users(firstname, surname, username, email, password) VALUES($1, $2, $3, $4, $5)`;
+		return db.query(sql, [firstname, surname, username, email, hashedPassword]).then((dbRes) => dbRes);
 	},
 };
 
