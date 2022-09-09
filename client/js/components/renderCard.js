d52bd080219aae86079c85173f81a4ddf7586cea
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-09 22:44:23 +1000

    restyling of card buttons + changed momentjs import to its jsdeliver counterpart + added test function where the actual ones will go

--- client/js/components/renderCard.js
+++ client/js/components/renderCard.js
@@ -2,12 +2,12 @@ const renderCard = (holiday) => {
 	const cardContainer = document.createElement("div");
 	cardContainer.className = "card-container";
 	cardContainer.innerHTML = `
-					<div class="card-top">
+					<div class="card-top" onclick="alert('render holiday_parts')">
 						<img alt=${holiday.location_name} src=${holiday.img_link}></img>
 					</div>
 					<div class="card-bottom">
 						<div>
-							<h2>${holiday.holiday_name}</h2>
+							<h2><a onClick="alert('render holiday_parts')">${holiday.holiday_name}</a></h2>
 							<h4>${holiday.location_name}</h4>
 							<p>${moment(holiday.date_start).format("MMM Do YY")} - ${moment(
 		holiday.date_start
@@ -15,6 +15,7 @@ const renderCard = (holiday) => {
 						</div>
 						<div class="btn-bottom">
 							<button id="edit-btn" onClick="alert('edit function')">Edit</button>
+							<div class="line"></div>
 							<button id="delete-btn" onClick="alert('delete function')">Delete</button>
 						</div>
 					</div>
