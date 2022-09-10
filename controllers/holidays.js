0040eb074a710a4a85bba7379fd7655e9e3b474b
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-10 17:55:54 +1000

    fixing userid

--- controllers/holidays.js
+++ controllers/holidays.js
@@ -9,13 +9,7 @@ router.get("/", (req, res) => {
 		return res.status(401).send({ message: "Not logged in" });
 	}
 
-	Holidays.getAllForUser(userId).then((holidayRows) => {
-		if (holidayRows.length > 0) {
-			res.json(holidayRows).status(200);
-		} else {
-			res.json(holidayRows).status(204);
-		}
-	});
+	Holidays.getAllForUser(userId).then((holidayRows) => res.json(holidayRows));
 });
 
 router.delete("/:id", (req, res) => {
@@ -25,77 +19,6 @@ router.delete("/:id", (req, res) => {
 		.catch((error) => {
 			res.status(500).json({ message: "holiday does not exist" });
 		});
-});
-
-router.post("/", (req, res) => {
-    //if you are working on the holidays page,
-    //change this to a hard coded id
-    //i.e. userId = 1;
-    const userId = req.session.userid;
-
-    if (!userId) {
-		return res.status(401).send({ message: "Not logged in!" });
-	}
-
-    //just creates the variables of the following names with their respective values
-    const { holiday_name, date_start, date_end, location_name, img_link } = req.body;
-
-    if(holiday_name === ''
-        || date_start === '' 
-        || date_end === '' 
-        || location_name === '' 
-        || img_link === '' ) {
-            return res.status(400).json({ message: 'Please fill in the entire form.' });
-        }
-
-    //this just stores the req.body contents within the data object
-    // i.e. data = {
-    // userId: 1,
-    // holiday_name: 'Paris'
-    // date_start: '1997-07-28'
-    //etc...etc...
-    // }
-    const data = { userId, ...req.body };
-
-    Holidays.add(data)
-    .then(() => res.status(200).json({ success: true }))
-    .catch(() => res.status(500).json({ success: false }));
-});
-
-router.put("/:id", (req, res) => {
-    //if you are working on the holidays page,
-    //change this to a hard coded id
-    //i.e. userId = 1;
-    const userId = req.session.userid;
-
-    if (!userId) {
-		return res.status(401).send({ message: "Not logged in!" });
-	}
-
-    //just creates the variables of the following names with their respective values
-    const { holiday_name, date_start, date_end, location_name, img_link } = req.body;
-    const holiday_id = req.params.id;
-
-    if(holiday_name === ''
-        || date_start === '' 
-        || date_end === '' 
-        || location_name === '' 
-        || img_link === '' ) {
-            return res.status(400).json({ message: 'Please fill in the entire form.' });
-        }
-
-    //this just stores the req.body contents within the data object
-    // i.e. data = {
-    // userId: 1,
-    // holiday_name: 'Paris'
-    // date_start: '1997-07-28'
-    //etc...etc...
-    // }
-    const data = { holiday_id, ...req.body };
-
-    Holidays.edit(data)
-    .then(() => res.status(200).json({ success: true }))
-    .catch(() => res.status(500).json({ success: false }));
 });
 
 //these are for testing via Postman
