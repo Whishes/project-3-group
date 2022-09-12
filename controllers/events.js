076a73cfbd8c5e19557edd60378b11e951c6fdf0
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-12 13:37:35 +1000

    deleteEvent route added

--- controllers/events.js
+++ controllers/events.js
@@ -11,7 +11,11 @@ router.get("/", (req, res) => {
 
 	const part_id = req.body.part_id;
 
-	Events.getAll(part_id).then((eventRows) => res.json(eventRows));
+	Events.getAll(part_id)
+		.then((eventRows) => res.json(eventRows))
+		.catch((err) => {
+			res.status(500).json({ message: "Could not get data" });
+		});
 });
 
 router.get("/:id", (req, res) => {
@@ -23,7 +27,24 @@ router.get("/:id", (req, res) => {
 		return res.status(401).send({ message: "Not logged in" });
 	}
 
-	Events.getOne(eventId).then((eventRows) => res.json(eventRows));
+	Events.getOne(eventId)
+		.then((eventRows) => res.json(eventRows))
+		.catch((err) => {
+			res.status(500).json({ message: "Could not get data" });
+		});
+});
+
+router.delete("/:id", (req, res) => {
+	const userId = req.session.userid;
+	const eventId = req.params.id;
+
+	if (!userId) {
+		return res.status(401).send({ message: "Not logged in" });
+	}
+
+	Events.deleteOne(eventId)
+		.then(() => res.json({ success: true }))
+		.catch((err) => res.status(500).json({ message: "Delete Unsuccessful" }));
 });
 
 module.exports = router;
--- models/events.js
+++ models/events.js
@@ -2,12 +2,16 @@ const db = require("../database/db.js");
 
 const Events = {
 	getAll: (part_id) => {
-		const sql = `SELECT * FROM events WHERE part_id=${part_id}`;
-		return db.query(sql).then((dbRes) => dbRes.rows);
+		const sql = "SELECT * FROM events WHERE part_id=$1";
+		return db.query(sql, [part_id]).then((dbRes) => dbRes.rows);
 	},
 	getOne: (event_id) => {
-		const sql = `SELECT * FROM events WHERE id=${event_id}`;
-		return db.query(sql).then((dbRes) => dbRes.rows);
+		const sql = "SELECT * FROM events WHERE id=$1";
+		return db.query(sql, [event_id]).then((dbRes) => dbRes.rows);
+	},
+	deleteOne: (eventId) => {
+		const sql = `DELETE FROM events WHERE id = $1`;
+		return db.query(sql, [eventId]).then((dbRes) => dbRes.rows);
 	},
 };
 
