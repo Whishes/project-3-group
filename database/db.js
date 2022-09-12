5f7cd6f42cc9cb4b0a2c03da930fc94420d0ae35
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-12 21:58:03 +1000

    maybe now

--- database/db.js
+++ database/db.js
@@ -1,10 +1,10 @@
 const pg = require("pg");
 require("dotenv").config();
-//const connectionString = process.env.CONNECTION_STRING;
+const connectionString = process.env.DATABASE_URL;
 
 const db = new pg.Pool({
 	database: "travel_diary",
-	//connectionString,
+	connectionString,
 });
 
 module.exports = db;
