6119a35beb580e5e7b4921d7b3998cd4054a84dc
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-12 17:41:24 +1000

    dumb changes but getting dinner for a hot minute so ya know

--- client/js/components/profile.js
+++ client/js/components/profile.js
@@ -1,3 +1,36 @@
 export const renderUserProfile = (userId) => {
-	const sectionPage = document.getElementById("page");
+	const page = document.getElementById("page");
+	const formContainer = document.createElement("div");
+	formContainer.id = "registration";
+	formContainer.innerHTML = `
+            <form id="registration-form" action="">
+                <h2>Your Profile!</h2>
+                <p>Feel free to change anything you'd like</p>                   
+                    <div id="name-container">
+                        <div class="div-input-field name-field">
+                            <i class="fa-solid fa-user icon"></i>
+                            <input id="firstname-input" type="text" name="firstname" placeholder="First Name" required minlength="1">
+                        </div>
+                        
+                        <div class="div-input-field name-field" id="surname-div">
+                            <input id="surname-input" type="text" name="surname" placeholder="Surname" required minlength="1">
+                        </div>
+                    </div>
+                    <div class="div-input-field">
+                        <i class="fa-solid fa-face-smile icon"></i>
+                        <input class="long-input" type="text" name="username" placeholder="Username" required minlength="3">
+                    </div>
+                   
+                    <div class="div-input-field">
+                        <i class="fa-solid fa-envelope icon"></i>
+                        <input class="long-input" type="email" name="email" placeholder="Email" required>
+                    </div>   
+					<div id="registration-base">
+                       <button id="register-btn" style="width: 80%">Save Settings</button> 
+                    </div>   
+            </form>
+`;
+	page.replaceChildren(formContainer);
+
+	// do the axios request to get user data then render the form with all fields filled in
 };
--- client/js/components/registration.js
+++ client/js/components/registration.js
@@ -1,7 +1,7 @@
 const renderRegistrationForm = () => {
-    const page = document.getElementById("page");
-    const form = document.createElement("form");
-    form.innerHTML = `
+	const page = document.getElementById("page");
+	const form = document.createElement("form");
+	form.innerHTML = `
         <section id="registration">
             <form id="registration-form" action="">
 
@@ -38,35 +38,40 @@ const renderRegistrationForm = () => {
                     <span>Already have an account?</span> <button id="register">Register</button> 
                     </div>
                   
-            </form>
-            
-        </section>`
+            </form>`;
 
-        form.addEventListener("submit", (event) => {
-            event.preventDefault();
-            const formData = new FormData(form);
-            const data = {
-                firstname: formData.get("firstname"),
-                surname: formData.get("surname"),
-                username: formData.get("username"),
-                email: formData.get("email"),
-                password: formData.get("password")
-            };
+	form.addEventListener("submit", (event) => {
+		event.preventDefault();
+		const formData = new FormData(form);
+		const data = {
+			firstname: formData.get("firstname"),
+			surname: formData.get("surname"),
+			username: formData.get("username"),
+			email: formData.get("email"),
+			password: formData.get("password"),
+		};
 
-            if (!data.firstname || !data.surname || !data.email || !data.password || !data.username) {
-                return alert( "one of the fields is empty");
-            } else {
-                axios.post("api/signup", data).then(() => {
-                    location = "/";
-                }).catch((err) => {
-                errorMsg.textContent = err.response.data.message;
-                });
-            }});
-        
-        page.replaceChildren(form);
-    };
+		if (
+			!data.firstname ||
+			!data.surname ||
+			!data.email ||
+			!data.password ||
+			!data.username
+		) {
+			return alert("one of the fields is empty");
+		} else {
+			axios
+				.post("api/signup", data)
+				.then(() => {
+					location = "/";
+				})
+				.catch((err) => {
+					errorMsg.textContent = err.response.data.message;
+				});
+		}
+	});
 
-        
-       
+	page.replaceChildren(form);
+};
 
 module.exports = renderRegistrationForm;
