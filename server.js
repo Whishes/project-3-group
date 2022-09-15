cd803a59f381cd0c13ba3c422755c202f37458d8
Nicholas <nicholas.cruickshank25@gmail.com>
2022-09-15 18:54:13 +1000

    trying a change that Ken suggested for heroku

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const { expressSession, pgSession } = require("./session");
 const app = express();
 const port = process.env.PORT;
 
+// serving frontend stuff
+app.use(express.static("client"));
+app.use(express.json());
+
 // getting session info working into the db
 app.use(
 	expressSession({
@@ -28,19 +32,16 @@ app.use(
 		secret: process.env.EXPRESS_SESSION_SECRET_KEY,
 	})
 );
+
+// cache stuff
 app.use(nocache());
 app.set("etag", false);
-
-// serve the frontend
-app.use(express.json());
-app.use(express.static("client"));
 
 // routes
 app.use("/api/signup", signupController);
 app.use("/api/session", sessionController);
 app.use("/api/holidays", holidaysController);
 app.use("/api/holidayparts", holidayPartsController);
-
 app.use("/api/events", eventsController);
 
 // start the web server
