238f1930eea7e064366fe3fcf2de8950b0dc8f48
Elise James <elisabethmariajames@gmail.com>
2022-09-15 16:23:10 +1000

    renderNewTrip function is working to add holiday and add a holiday part now

--- client/js/components/newTrip.js
+++ client/js/components/newTrip.js
@@ -89,6 +89,96 @@ export const renderNewTrip = (what, holidayId) => {
 				});
 		});
 	} else if (what === "holiday_part") {
-		alert(`Add part ${holidayId}`)
+		// alert(`Add part ${holidayId}`)
+
+		page.innerHTML = `
+		<section id="newtrip">
+		<form id="newtrip-form">
+
+			<h2>Add a new holiday part!</h2>
+
+			<input name="holiday_id" class="long-input" type="hidden" value=${holidayId}>
+
+			<div class="div-input-field">
+				<i class="fa-solid fa-suitcase icon"></i>
+				<input name="part_name" class="long-input" type="text" placeholder="Holiday Part Name">
+			</div>
+
+			<div class="div-input-field">
+				<i class="fa-solid fa-earth-americas icon"></i>
+				<input name="location_name" class="long-input" type="text" placeholder="Location">
+			</div>
+
+			<div class="div-input-field">
+				<i class="fa-solid fa-image icon"></i>
+				<input name="img_link" class="long-input" type="text" placeholder="Location Image Link">
+			</div>
+
+			<div class="div-input-field date-field">
+				<i class="fa-solid fa-plane-departure icon"></i>
+				<input name="date_start" class="date-input" type="text" placeholder="Start Date">
+			</div>
+
+			<div class="div-input-field date-field">
+				<i class="fa-solid fa-plane-arrival icon"></i>
+				<input name="date_end" class="date-input" type="text" placeholder="End Date">
+			</div>
+
+			<div id="newtrip-base">
+				<button id="create" type="submit">Create</button>
+			</div>
+
+		</form>
+		</section>
+		`;
+
+		//makes the UI of the start/end dates more responsive
+		const dateInputs = document.querySelectorAll(".date-input");
+
+		for (let input of dateInputs) {
+			input.addEventListener("focus", (event) => {
+				event.target.type = "date";
+				event.target.showPicker();
+			});
+			input.addEventListener("blur", (event) => {
+				event.target.type = "text";
+			});
+		}
+
+		//code to post the form data
+		const newTripForm = document.querySelector("#newtrip-form");
+
+		newTripForm.addEventListener("submit", (event) => {
+			event.preventDefault();
+			const formData = new FormData(newTripForm);
+			const data = {
+				holiday_id: formData.get("holiday_id"),
+				part_name: formData.get("part_name"),
+				date_start: formData.get("date_start"),
+				date_end: formData.get("date_end"),
+				location_name: formData.get("location_name"),
+				img_link: formData.get("img_link"),
+			};
+
+			for (let value in data) {
+				if (!value) {
+					return alert("Please fill out entire form");
+				}
+			}
+
+			if (data.date_start >= data.date_end) {
+				return alert("Ensure that your dates are correct");
+			}
+
+			axios
+				.post("/api/holidayparts", data)
+				.then(() => {
+					location.href = "/";
+				})
+				.catch((err) => {
+					return alert(err);
+					//errorBar(err.response.data.message, "error");
+				});
+		});
 	}
 };
